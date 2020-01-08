929c50ef63948ed1ae592bca4b8130a8303dbce6
Alexa Dominguez <alexadominguez@Alexas-MacBook-Pro.local>
2020-01-08 11:17:58 -0600

    Updates

---
+++ precache-manifest.2b6cb1a2ea8fd62d4bf7c22c6f560742.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc359349ccba365d211ec932f6d8c089",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "c615f9820ee8549765aa",
+    "url": "/pokedex/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6ff7787c34ae083625b0",
+    "url": "/pokedex/static/js/2.f5524f6c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pokedex/static/js/2.f5524f6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c615f9820ee8549765aa",
+    "url": "/pokedex/static/js/main.a05af237.chunk.js"
+  },
+  {
+    "revision": "966bc7290b5d2e947473",
+    "url": "/pokedex/static/js/runtime-main.3840d353.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokedex/static/media/logo.5d5d9eef.svg"
+  }
+]);
